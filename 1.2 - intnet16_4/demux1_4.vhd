7062e17b8ef61463683cb7a58a9c5aca0fc4d474
Rasbon99 <lollo.manco@gmail.com>
2023-11-28 20:41:40 +0100

    Update demux1_4.vhd
    Wrong demux execution interpretation, edit dataflow
    
    Co-Authored-By: Leonardo Catello <leonardo.catello23@gmail.com>

--- 1.2 - intnet16_4/demux1_4.vhd
+++ 1.2 - intnet16_4/demux1_4.vhd
@@ -17,16 +17,24 @@ entity demux1_4 is
            y : out STD_LOGIC_VECTOR (3 downto 0));
 end demux1_4;
 
-architecture Behavioral of demux1_4 is
+-- questa prima soluzione non tiene conto della variazione indefinita 
+-- dello stato in quanto non ha una condizione di default
+architecture dataflow_1 of demux1_4 is
     begin
-        process(s, a) is
-            begin
-                if (s = "00") then y(0) <= a; 
-                elsif (s = "01") then y(1) <= a;
-                elsif (s = "10") then y(2) <= a;
-                elsif (s = "11") then y(3) <= a;
-                else y <= "----"; 
-                end if;
-        end process;
+        y(0) <= a when s = "00" else '0';
+        y(1) <= a when s = "01" else '0';
+        y(2) <= a when s = "10" else '0';
+        y(3) <= a when s = "11" else '0';
+end dataflow_1;
 
-end Behavioral;
+-- soluzione più robusta, prevede un caso di default
+architecture dataflow_2 of demux1_4 is
+begin
+    with s select
+        y <= ('0', '0', '0', a) when "00",
+             ('0', '0', a, '0') when "01",
+             ('0', a, '0', '0') when "10",
+             (a, '0', '0', '0') when "11",
+             (others => '0') when others;
+
+end dataflow_2;
