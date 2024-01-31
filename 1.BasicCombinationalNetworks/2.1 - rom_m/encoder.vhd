7c2f8e36211eea6d67cdb5d1532524ead8762188
Rasbon99 <lollo.manco@gmail.com>
2024-01-31 16:05:08 +0100

    Update encoder.vhd
    Co-Authored-By: Leonardo Catello <leonardo.catello23@gmail.com>
    Co-Authored-By: ilaria saulino <ilarysaul@gmail.com>

--- 1.BasicCombinationalNetworks/2.1 - rom_m/encoder.vhd
+++ 1.BasicCombinationalNetworks/2.1 - rom_m/encoder.vhd
@@ -7,23 +7,10 @@ entity encoder is
            z : out STD_LOGIC_VECTOR (3 downto 0));
 end encoder;
 
-architecture Behavioral of encoder is
+architecture Dataflow of encoder is
     begin    
-        z <=    "1111" when x = "00000000" else
-                "1110" when x = "00000001" else
-                "1101" when x = "00000010" else
-                "1100" when x = "00000011" else
-                "1011" when x = "00000100" else
-                "1010" when x = "00000101" else 
-                "1001" when x = "00000110" else
-                "1000" when x = "00000111" else
-                "0111" when x = "00001000" else
-                "0110" when x = "00001001" else
-                "0101" when x = "00001010" else
-                "0100" when x = "00001011" else
-                "0011" when x = "00001100" else
-                "0010" when x = "00001101" else
-                "0001" when x = "00001110" else
-                "0000" when x = "00001111" else
-                "----";
-end Behavioral;
+        z(0) <= '0' when x(0) = '1' else '1';
+        z(1) <= '0' when x(1) = '1' else '1';
+        z(2) <= '0' when x(2) = '1' else '1';
+        z(3) <= '0' when x(3) = '1' else '1';
+end Dataflow;
