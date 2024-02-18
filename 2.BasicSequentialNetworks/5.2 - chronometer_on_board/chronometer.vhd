d95b3527240357ec0fcfe51c3baf5e84ac0a69a2
Rasbon99 <lollo.manco@gmail.com>
2024-02-18 17:03:24 +0100

    Update chronometer on board
    Fixed the same chronometer issue

--- 2.BasicSequentialNetworks/5.2 - chronometer_on_board/chronometer.vhd
+++ 2.BasicSequentialNetworks/5.2 - chronometer_on_board/chronometer.vhd
@@ -24,83 +24,60 @@ architecture Structural of chronometer is
                 reset_cmn : in STD_LOGIC;
                 input_cmn : in integer := 0;
                 load_cmn : in STD_LOGIC;
+                enable_cmn : in STD_LOGIC;
                 q_cmn : out integer := 0;
                 co_cmn : out STD_LOGIC);
     end component;
-    
-    component mux2_1 is
-        Port ( a : in STD_LOGIC_VECTOR (1 downto 0);
-               s : in STD_LOGIC;
-               y : out STD_LOGIC);
-    end component;
-    
+
     signal co_nano : STD_LOGIC := '0';
     signal co_sec : STD_LOGIC := '0';
     signal co_min : STD_LOGIC := '0';
-    signal co_hour : STD_LOGIC := '0';
+    signal co_hour : STD_LOGIC := '0';    
     
-    -- new_day: reset interno, controllato dall'ultimo
-    -- contatore delle ore che arrivato a 23 si azzera
-    -- e alza il seganle di new day -> reset per tutti
-    -- i precedenti coontatori.
-    signal new_day : STD_LOGIC := '0';
-    
-    -- mux_out: wire di collegamento tra l'uscita del
-    -- mux e l'ingresso dei contatori. 
-    signal mux_out : STD_LOGIC_VECTOR (2 downto 0);
-
     begin
-    
-        -- Mux controllati dal segnale di load, permettono di abilitare
-        -- i contatori mettendo in ingresso a tutti il clock e ponendo
-        -- valori di input per l'orario desiderato. Il contatore non
-        -- avanza perchè per load alto carica il valore di input.
-    
-        mux0 : mux2_1 port map (a(0) => co_nano, a(1) => clk, s => load, y => mux_out(0));
-        mux1 : mux2_1 port map (a(0) => co_sec, a(1) => clk, s => load, y => mux_out(1));
-        mux2 : mux2_1 port map (a(0) => co_min, a(1) => clk, s => load, y => mux_out(2));
-        
 --        Questo contatore genera il conteggio di 1 s
---        Essendo la f = 100MHz del clock, quindi un T = 10 ns
+--        Dato che la f = 100MHz del clock, quindi un T = 10 ns
 --        ho bisogno di un contatore mod-10^8 perchè 1s = 10^8*T
 --        quindi mi servono log2(10^8) ~ 27 bit -> flip-flop,
 --        di conseguenza a 10^8 - 1 = 101111101011110000011111111 dò
---        il segnale di reset = '1' creando un contatore per riduzione
---        ad incremento costante pari a 1.
+--        il segnale di co_nano = '1' e azzero il conteggio, 
+--        creando un contatore per riduzione ad incremento costante.
     
         nano_counter : counter_mod_n generic map (N_cmn => 27, selection_cmn => 0) port map(
             CLK_cmn => clk,
-            reset_cmn => new_day,
+            reset_cmn => reset,
             input_cmn => 0,
             load_cmn => load,
+            enable_cmn => '1',
             q_cmn => nano_output,
             co_cmn => co_nano);
         
         sec_counter : counter_mod_n generic map (N_cmn => 6, selection_cmn => 1) port map(
-            CLK_cmn => mux_out(0),             
-            reset_cmn => new_day,
+            CLK_cmn => clk,             
+            reset_cmn => reset,
             input_cmn => sec_input,
             load_cmn => load,
+            enable_cmn => co_nano,
             q_cmn => sec_output,        
             co_cmn => co_sec);
               
         min_counter : counter_mod_n generic map (N_cmn => 6, selection_cmn => 1) port map(
-            CLK_cmn => mux_out(1),
-            reset_cmn => new_day,
+            CLK_cmn => clk,
+            reset_cmn => reset,
             input_cmn => min_input,
             load_cmn => load,
+            enable_cmn => co_sec,
             q_cmn => min_output,        
             co_cmn => co_min);
 
         hour_counter : counter_mod_n generic map (N_cmn => 5, selection_cmn => 2) port map(
-            CLK_cmn => mux_out(2),
-            reset_cmn => new_day,
+            CLK_cmn => clk,
+            reset_cmn => reset,
             input_cmn => hour_input,
             load_cmn => load,
+            enable_cmn => co_min,
             q_cmn => hour_output,        
             co_cmn => co_hour);
-            
-        new_day <= reset or co_hour;
-            
+                        
 
 end Structural;
--- 2.BasicSequentialNetworks/5.2 - chronometer_on_board/counter_mod_n.vhd
+++ 2.BasicSequentialNetworks/5.2 - chronometer_on_board/counter_mod_n.vhd
@@ -1,51 +1,61 @@
-
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;
-
-entity counter_mod_n is
-    Generic(N_cmn : integer := 6;
-            selection_cmn : integer range 0 to 2);                             
-    Port (  CLK_cmn : in STD_LOGIC;
-            reset_cmn : in STD_LOGIC;
-            input_cmn : in integer := 0;
-            load_cmn : in STD_LOGIC;
-            q_cmn : out integer := 0;
-            co_cmn : out STD_LOGIC);
-end counter_mod_n;
-
-architecture Behavioral of counter_mod_n is
-    signal temp : STD_LOGIC_VECTOR(N_cmn - 1 downto 0);
-    signal internal_reset : STD_LOGIC;
-    signal co_interrupt : STD_LOGIC_VECTOR (N_cmn - 1 downto 0);    
-
-    begin
     
-        with selection_cmn select
-            co_interrupt <= "101111101011110000011111111" when 0,       -- interruzione del counter 1s = 99999999 
-                            "111011" when 1,                            -- interruzione del counter 1 min/1 hour = 59 
-                            "10111" when 2;                             -- interruzione del counter 1 gg = 23
-
-        counter_proc : process (CLK_cmn, reset_cmn)
-            begin
-                if (reset_cmn = '1') then
-                        temp <= (others => '0');
-                        co_cmn <= '0';
-                elsif rising_edge(CLK_cmn) then
-                        if (temp = co_interrupt) then
-                            temp <= (others => '0');
-                            co_cmn <= '1';
+    library IEEE;
+    use IEEE.STD_LOGIC_1164.ALL;
+    use IEEE.NUMERIC_STD.ALL;
+    
+    -- CONTATORE MOD N, GENERICO PER ESSERE ISTANZIATO COME:
+    -- - NANO_COUNTER, con selection_cmn = 0 e N = 27 -> conteggio fino a 99999999
+    -- - SEC_COUNTER, con selection_cmn = 1 e N = 6 -> conteggio fino a 59
+    -- - MIN_COUNTER, con selection_cmn = 1 e N = 6 -> conteggio fino a 59
+    -- - HOUR_COUNTER, con selection_cmn = 2 e N = 5 -> conteggio fino a 23
+    
+    entity counter_mod_n is
+        Generic(N_cmn : integer := 6;
+                selection_cmn : integer range 0 to 2);                             
+        Port (  CLK_cmn : in STD_LOGIC;
+                reset_cmn : in STD_LOGIC;
+                input_cmn : in integer := 0;
+                load_cmn : in STD_LOGIC;
+                enable_cmn : in STD_LOGIC;
+                q_cmn : out integer := 0;
+                co_cmn : out STD_LOGIC);
+    end counter_mod_n;
+    
+    architecture Behavioral of counter_mod_n is
+        signal count_temp : STD_LOGIC_VECTOR(N_cmn - 1 downto 0);
+        signal co_interrupt : STD_LOGIC_VECTOR (N_cmn - 1 downto 0);
+    
+        begin
+        
+            with selection_cmn select
+                co_interrupt <= "101111101011110000011111111" when 0,       -- interruzione del counter 1s = 99999999 
+                                "111011" when 1,                            -- interruzione del counter 1 min/1 hour = 59 
+                                "10111" when 2;                             -- interruzione del counter 1 gg = 23
+    
+            counter_proc : process (CLK_cmn)
+                begin
+                    if rising_edge(CLK_cmn) then
+                        if (reset_cmn = '1') then
+                            count_temp <= (others => '0');
                         elsif (load_cmn = '1') then
-                            temp <= std_logic_vector(to_unsigned(input_cmn, N_cmn));
-                            co_cmn <= '0';
-                        else
-                            temp <= std_logic_vector(unsigned(temp) + 1);
-                            co_cmn <= '0';
-                        end if;                        
-                end if;
-        end process;
-        
-        
-        q_cmn <= to_integer(unsigned(temp));
-
-end Behavioral;
+                            count_temp <= std_logic_vector(to_unsigned(input_cmn, N_cmn));
+                        elsif (enable_cmn = '1') then
+                            if (count_temp = co_interrupt) then
+                                count_temp <= (others => '0');
+                            else
+                                count_temp <= std_logic_vector(unsigned(count_temp) + 1);
+                            end if; 
+                        end if;     
+                    end if;
+            end process;
+    
+            q_cmn <= to_integer(unsigned(count_temp));
+            
+            -- co_cmn: segnale di conclusione di conteggio, è l'abilitazione del
+            -- prossimo contatore: sec, min, hour. Solo nano ha sempre l'abilitazione alta.
+            -- L'aggiornamento è posto fuori al process in quanto vogliamo che il 
+            -- segnale si attivi esattamente alla verifica della condizione di fine.
+            
+            co_cmn <= '1' when count_temp = co_interrupt and enable_cmn = '1' else '0';
+    
+    end Behavioral;
