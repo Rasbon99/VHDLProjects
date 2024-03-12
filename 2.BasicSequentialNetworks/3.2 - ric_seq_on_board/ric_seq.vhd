ba9962a63533c82862c12ed28fd77427eb1257c0
Rasbon99 <lollo.manco@gmail.com>
2024-03-12 12:11:54 +0100

    Update ric_seq on board

--- 2.BasicSequentialNetworks/3.2 - ric_seq_on_board/ric_seq.vhd
+++ 2.BasicSequentialNetworks/3.2 - ric_seq_on_board/ric_seq.vhd
@@ -1,91 +1,114 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 -- RICONOSCITORE DI SEQUENZA 101
 entity ric_seq is
     Port ( i : in STD_LOGIC;         -- INPUT
            R, A : in STD_LOGIC;      -- RESET E CLOCK
            M : in STD_LOGIC;         -- MODO
+           E : in STD_LOGIC;         -- ENABLE
            Y : out STD_LOGIC);       -- OUTPUT
 end ric_seq;
 
 architecture Behavioral of ric_seq is
     type stato is (S0, S1, S2, S3, S4); 
     signal stato_corrente : stato := S0;
-    signal stato_prossimo : stato;
+    signal mode : STD_LOGIC := '0';
+begin
     
-    begin
-    
-    stato_uscita: process(stato_corrente, i)
-    begin 
-        case stato_corrente is
-        
-            -- MODE 0 SEQUENZE NON SOVRAPPOSTE
-                -- S0 - S1 - S2 - S3 - S4
-            -- MODE 1 SEQUENZE PARZIALMENTE SOVRAPPOSTE
-                -- S0 - S1 - S2
-            
-            when S0 => 
-                if (i = '1') then 
-                    stato_prossimo <= S1;   
-                    Y <= '0';                   -- RIMANE IN 0
-                elsif (M = '0') then 
-                    stato_prossimo <= S3;
-                    Y <= '0';                   -- RIMANE IN 0
-                else 
-                    stato_prossimo <= S0;      
-                    Y <= '0';                   -- RIMANE IN 0
-                end if;
-    
-            when S1 => 
-                if (M = '1' and i = '1') then 
-                    stato_prossimo <= S1;    
-                    Y <= '0';                   -- RIMANE IN 0
-                elsif (M = '0' and i = '0') then 
-                    stato_prossimo <= S4;
-                    Y <= '0';                   -- RIMANE IN 0
-                else 
-                    stato_prossimo <= S2;     
-                    Y <= '0';                   -- RIMANE IN 0
-                end if;
+    FSM: process (A)
+        begin
+            if( A'event and A = '1' ) then
+                if( R = '1') then
                 
-            when S2 =>  
-                if (M = '1' and i = '1') then
-                    stato_prossimo <= S0;                           
-                    Y <= '1';               -- SET M = 1
-                else  
-                    stato_prossimo <= S0;                                                  
-                    Y <= '0';               -- RESET
+                    if (M = '0') then
+                        mode <= '0';
+                    elsif (M = '1') then
+                        mode <= '1';
+                    end if;
+                    stato_corrente <= S0;
+                else
+                    if mode = '0' then
+                        case stato_corrente is
+                            when S0 => 
+                                if (i = '1' and E = '1') then
+                                    Y <= '0';
+                                    stato_corrente <= S1;               
+                                elsif (E = '1') then
+                                    Y <= '0';
+                                    stato_corrente <= S3;
+                                end if;
+                
+                            when S1 => 
+                                if (i = '0' and E = '1') then
+                                    Y <= '0';
+                                    stato_corrente <= S4;              
+                                elsif (E = '1') then
+                                    Y <= '0';
+                                    stato_corrente <= S2; 
+                                end if;
+                                
+                            when S2 =>
+                                if (E = '1') then
+                                    Y <= '0';
+                                    stato_corrente <= S0;
+                                end if;    
+                
+                            when S3 => 
+                                if (E = '1') then
+                                    Y <= '0';
+                                    stato_corrente <= S2;
+                                end if;     
+                
+                            when S4 => 
+                                if (i = '1' and E = '1') then 
+                                    stato_corrente <= S0;                                        
+                                    Y <= '1';               
+                                elsif (E = '1') then
+                                    Y <= '0';
+                                    stato_corrente <= S0;            
+                                end if;
+                                
+                        end case;
+                        
+                    elsif (mode = '1') then
+                        -- MODE 1 SEQUENZE PARZIALMENTE SOVRAPPOSTE
+                            -- S0 - S1 - S2
+                        case stato_corrente is
+                            when S0 => 
+                                if (i = '1' and E = '1') then
+                                    Y <= '0';
+                                    stato_corrente <= S1;                   
+                                elsif (E = '1') then
+                                    Y <= '0';
+                                    stato_corrente <= S0;           
+                                end if;
+                
+                            when S1 => 
+                                if (i = '1' and E = '1') then
+                                    Y <= '0';
+                                    stato_corrente <= S1;          
+                                elsif (E = '1') then
+                                    Y <= '0';
+                                    stato_corrente <= S2;                     
+                                end if;
+                                
+                            when S2 =>  
+                                if (i = '1' and E = '1') then
+                                    stato_corrente <= S0;                           
+                                    Y <= '1';               
+                                elsif( E = '1') then
+                                    Y <= '0';
+                                    stato_corrente <= S0;                
+                                end if;
+                             
+                             when others =>
+                                Y <= '0';
+                                stato_corrente <= S0;
+                        end case;
+                    end if;
                 end if;
-    
-            when S3 => 
-                stato_prossimo <= S2;                                                
-                Y <= '0';                    -- RIMANE IN 0
-    
-            when S4 => 
-                if i = '1' then 
-                    stato_prossimo <= S0;                                        
-                    Y <= '1';               -- SET M = 0
-                else               
-                    stato_prossimo <= S0;                                                 
-                    Y <= '0';               -- RESET
-                end if;
-    
-        end case;
-    
-    end process;
-    
-    mem: process (A)
-    begin
-        if( A'event and A = '1' ) then
-            if( R = '1') then
-                stato_corrente <= S0;
-            else
-                stato_corrente <= stato_prossimo;
             end if;
-        end if;
-    end process;
-    
+        end process;
+        
 end Behavioral;
--- 2.BasicSequentialNetworks/3.2 - ric_seq_on_board/ric_seq_on_board.vhd
+++ 2.BasicSequentialNetworks/3.2 - ric_seq_on_board/ric_seq_on_board.vhd
@@ -4,9 +4,9 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity ric_seq_on_board is
     Port (  S1, S2 : in STD_LOGIC;                  -- INPUT E MODO
-            CLOCK : in STD_LOGIC;                     --  CLOCK
-            LED, LED1, LED2 : out STD_LOGIC;                    -- OUTPUT
-            B1, B2, BTN_RST : in STD_LOGIC);        -- BTN SX, BTN DX, BTN CPU RESET
+            CLOCK : in STD_LOGIC;                   -- CLOCK
+            LED: out STD_LOGIC;                     -- OUTPUT
+            BTN, BTN_RST : in STD_LOGIC);        -- BTN SX (MODO, INPUT), BTN CPU RESET
 end ric_seq_on_board;
 
 architecture Strucural of ric_seq_on_board is
@@ -15,85 +15,41 @@ architecture Strucural of ric_seq_on_board is
         Port ( i : in STD_LOGIC;         -- INPUT
                R, A : in STD_LOGIC;      -- RESET E CLOCK
                M : in STD_LOGIC;         -- MODO
+               E : in STD_LOGIC;         -- ENABLE
                Y : out STD_LOGIC);       -- OUTPUT
     end component;
     
     component ButtonDebouncer is
-    generic(CLK_period: integer := 10;  -- periodo del clock in nanosec
-            btn_noise_time: integer := 10000000); -- durata dell'oscillazione in nanosec
-    Port (  RST : in STD_LOGIC;
-            CLK : in STD_LOGIC;
-            BTN : in STD_LOGIC; 
-            CLEARED_BTN : out STD_LOGIC);
+        generic(CLK_period: integer := 10;  -- periodo del clock in nanosec
+                btn_noise_time: integer := 10000000); -- durata dell'oscillazione in nanosec
+        Port (  RST : in STD_LOGIC;
+                CLK : in STD_LOGIC;
+                BTN : in STD_LOGIC; 
+                CLEARED_BTN : out STD_LOGIC);
     end component;
     
-    signal btn1_wire, btn2_wire, btnRst_wire : STD_LOGIC := '0';
-    signal clr_btn1, clr_btn2 : STD_LOGIC := '0'; 
+    signal clr_btn: STD_LOGIC := '0'; 
     signal enable : STD_LOGIC := '0';
     signal input_rs : STD_LOGIC := '0';
     signal mode_rs : STD_LOGIC := '0';
     signal reset : STD_LOGIC := '0';
     
-
     begin
         
         reset <= not BTN_RST;
     
         RIC_SEQ_0: ric_seq port map(
-            i => input_rs,
+            i => S1,
             R => reset,
-            A => enable,
-            M => mode_rs,
+            A => CLOCK,
+            E => clr_btn,
+            M => S2,
             Y => LED);
-            
             
         BTND_1: ButtonDebouncer generic map (CLK_period => 10, btn_noise_time => 10000000) port map(
             RST => reset,
             CLK => CLOCK,
-            BTN => B1,
-            CLEARED_BTN => clr_btn1);
-
-        BTND_2: ButtonDebouncer generic map (CLK_period => 10, btn_noise_time => 10000000) port map(
-            RST => reset,
-            CLK => CLOCK,
-            BTN => B2,
-            CLEARED_BTN => clr_btn2);
-            
-        input_proc: process(B1, B2)
-            begin
-                if (B1 = '1' or B2 = '1') then
-                    enable <= '1';
-                    if (B1 = '1') then
-                        input_rs <= S1;
-                    else
-                        mode_rs <= S2;
-                    end if;
-                else
-                    enable <= '0';
-                end if;
-        end process;
-        
---        led_output : process
---            begin
---                if (B1 = '1') then
---                    LED1 <= '1';
---                    wait for 10000 ms;
---                else
---                    LED1 <= '0';
---                end if;
+            BTN => BTN,
+            CLEARED_BTN => clr_btn);
                 
---                if (B2 = '1') then
---                    LED2 <= '1';
---                    wait for 10000 ms;
---                else
---                    LED2 <= '0';
---                end if;
---        end process;
-            
---        enable <= '1' when clr_btn1 = '1' or clr_btn2 = '1' else '0';
-        
---        input_rs <= S1 when clr_btn1 = '1';
-        
---        mode_rs <= S2 when clr_btn2 = '1';
-
 end Strucural;
